dc019bc7115507770f517c86a18441d4804f1032
Arpit <arpit_jain@live.com>
2022-02-08 14:10:47 +0530

    init: first commit

---
+++ REFERENCES.md
@@ -0,0 +1,5 @@
+https://developer.mozilla.org/en-US/docs/Web/API
+https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+https://developer.mozilla.org/en-US/docs/Web/CSS/Reference
+https://caniuse.com
+https://css-tricks.com
---
+++ main.js
@@ -0,0 +1,13 @@
+const header = document.getElementById("header");
+const footer = document.getElementById("footer");
+console.log(document.querySelectorAll("[name='email']"));
+header.style.backgroundColor = "#888888";
+footer.style.color = "green";
+footer.classList.add('footer-class');
+// window.location = "https://google.com";
+// console.log(window);
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 150, 100);
