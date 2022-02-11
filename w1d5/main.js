06a3d0b3d2c86b27d6a8d978a6094ae141407c71
Arpit <arpit_jain@live.com>
2022-02-11 11:57:52 +0530

    feat: function expression and arrow function

--- w1d5/main.js
+++ w1d5/main.js
@@ -1,3 +1,4 @@
+// function declaration
 function add10(x) {
   const total = x + 10;
   return total;
@@ -10,3 +11,36 @@ const a3 = add10(); // no error for missing parameter
 
 const sum = add10(20);
 console.log(add10(20), sum);
+
+function substract(x, y) {
+  const result = x - y;
+  return result;
+}
+
+const r1 = substract(20, 10);
+const r2 = substract(200, 240);
+console.log(r1);
+console.log(r2);
+
+// function expression
+const multiply = function () {
+  const result = 10 * 20;
+  return result;
+}
+const difference = substract;
+console.log(multiply())
+console.log(difference(30, 10))
+console.log(typeof multiply)
+// when we don't provide a name to a function, it is called anonymous function
+
+// arrow function => introduced in es6 or ECMAScript2015
+const divide = () => {
+  const result = 120 / 5;
+  return result;
+};
+// demoDivide () => {
+//   const result = 120 / 5;
+//   return result;
+// }
+
+console.log(divide())
