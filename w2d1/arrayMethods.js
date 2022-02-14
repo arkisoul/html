f7a747ca11dc490d94bed720924a26d06af326c9
Arpit <arpit_jain@live.com>
2022-02-14 14:57:55 +0530

    feat: explain array methods map,reduce,filter etc

---
+++ w2d1/arrayMethods.js
@@ -0,0 +1,25 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const r1 = nums.forEach(function (element) {
+  console.log(`${element}`);
+  return element;
+});
+console.log(r1);
+const r2 = nums.map((ele) => ele * 3);
+
+console.log(r2, nums);
+const reversed = nums.reverse();
+console.log(reversed, nums);
+const numbers = [39, 12, 48, 29, 84, 36, 75, 45, 51];
+const sorted = numbers.sort((a, b) => {
+  if (a < b) return -1;
+  if (a === b) return 0;
+  return 1;
+});
+console.log(numbers, sorted);
+const filtered = numbers.filter((element) => element > 35 && element < 55);
+console.log(numbers, filtered);
+const total = nums.reduce(
+  (prevValue, currentValue) => prevValue * currentValue,
+  1
+);
+console.log(total, nums);
